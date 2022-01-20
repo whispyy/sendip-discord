ffcf04de4652db45431c73ba4b06bfbf3f140b15
JF Durand <jean-frederic.durand@qohash.com>
2022-01-20 12:33:53 +0100

    chore(*): fetch public ip v4/v6 and send it to hooks

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-require('dotenv').config();
+// require('dotenv').config()
 
-const { Webhook } = require('discord-webhook-node');
+// const { Webhook } = require('discord-webhook-node')
+// const publicIp = require('public-ip')
 
-const hook = new Webhook(process.env.WEBHOOK_URL);
-hook.setUsername(process.env.WEBHOOK_NAME);
-hook.setAvatar(process.env.IMAGE_URL);
- 
- 
-hook.send('Hello there!');
+import dotenv from 'dotenv'
+import { Webhook } from 'discord-webhook-node'
+import publicIp from 'public-ip'
+
+dotenv.config()
+
+// prepare hook
+const hook = new Webhook(process.env.WEBHOOK_URL)
+hook.setUsername(process.env.WEBHOOK_NAME)
+hook.setAvatar(process.env.IMAGE_URL)
+
+
+// fetch public ip
+const v4 = await publicIp.v4()
+const v6 = await publicIp.v6()
+
+// send message
+hook.send(`
+------
+Config
+------
+Date: ${ new Date()}
+
+IP v4: ${v4}
+IP v6: ${v6}
+`)
