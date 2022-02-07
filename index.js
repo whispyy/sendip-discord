64c58b27350956f1993c6a42794474695f071f0b
JF Durand <jean-frederic.durand@qohash.com>
2022-02-07 20:46:49 +0100

    feat(battery): send battery info in hooks

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-// require('dotenv').config()
-
-// const { Webhook } = require('discord-webhook-node')
-// const publicIp = require('public-ip')
-
 import dotenv from 'dotenv'
 import { Webhook } from 'discord-webhook-node'
 import publicIp from 'public-ip'
+import battery from 'battery'
 
 dotenv.config()
 
-// prepare hook
 const hook = new Webhook(process.env.WEBHOOK_URL)
 hook.setUsername(process.env.WEBHOOK_NAME)
 hook.setAvatar(process.env.IMAGE_URL)
 
-
-// fetch public ip
-const v4 = await publicIp.v4()
-const v6 = await publicIp.v6()
-
-// send message
-hook.send(`
+let message = `
 ------
 Config
 ------
-Date: ${ new Date()}
+Date: ${new Date()}
+`
 
-IP v4: ${v4}
-IP v6: ${v6}
-`)
+try {
+  const v4 = await publicIp.v4()
+  const v6 = await publicIp.v6()
+
+  message += `IP v4: ${v4}\nIP v6: ${v6}\n`
+} catch (e) {
+  message += `Error fetching ip ${e}`
+}
+
+
+try {
+  const { level, charging } = await battery()
+
+  message += `Battery: ${level*100}%, ${charging ? 'charging' : 'not charging'}\n`
+} catch(e) {
+  message += `Error fetching battery info ${e}`
+}
+
+hook.send(message)
